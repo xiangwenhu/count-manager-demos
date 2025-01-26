b537c0126e22a4ed0cabdb48786d48c109264cec
huxw <huxw@mail.jj.cn>
2025-01-26 16:01:53 +0800

    修改位Hash路由

--- docs/assets/Increase-2bTntAf_.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./Standard-DdHVZUqu.js";import{d as a,c as o,o as r}from"./index-CIuFpk-4.js";import"./index-Cb82_rL4.js";const f=a({__name:"Increase",setup(s){return(t,n)=>(r(),o(e,{options:{start:0*1e3,isDecrease:!1,end:60*1e3,notifyOnSubscribe:!1}}))}});export{f as default};
